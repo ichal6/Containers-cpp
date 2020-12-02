22b61e0fc8be4fb72c40d47fff58912d9d759a67
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-12-02 12:53:15 +0100

    Change visual output

--- src/HRMS.cpp
+++ src/HRMS.cpp
@@ -23,7 +23,7 @@ void HRMS::printDepartment(std::string departmentId) {
         if(listOfEmployeesId.empty()){
             std::cout << "The list is empty" << std::endl;
         }
-        std::cout << "List of employees for department " << departmentId;
+        std::cout << "\nList of employees for department " << departmentId;
         std::cout << "\nid\tname\tsurname\tposition" << std::endl;
         for(std::string employeeId: listOfEmployeesId){
             if(allEmployees.count(employeeId)){
@@ -46,7 +46,7 @@ void HRMS::printSalaries() {
     if(mapOfSalary.empty()){
         std::cout << "The list is empty" << std::endl;
     }
-    std::cout << "List of all employees" << std::endl;
+    std::cout << "\nList of all employees" << std::endl;
     std::cout << "\nid\tname\tsurname\tposition\tsalary" << std::endl;
     for(auto & singleEntry : allEmployees){
         std::cout << singleEntry.second << " " << mapOfSalary[singleEntry.first] << std::endl;
@@ -60,7 +60,7 @@ void HRMS::printSalariesSorted() {
 
     std::set<std::pair<std::string, double>, Comparator> sortMapOfSalary(mapOfSalary.begin(),
                                                                          mapOfSalary.end());
-    std::cout << "List of all employees sort by salary" << std::endl;
+    std::cout << "\nList of all employees sort by salary" << std::endl;
     std::cout << "\nid\tname\tsurname\tposition\tsalary" << std::endl;
 
     for(auto & singleEntry : sortMapOfSalary){
